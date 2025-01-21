c9b766a1b789ca2ea6e44122b0ea7710a034bb97
kcodes57 <kir652986@gmail.com>
2025-01-21 13:35:56 -0600

    added link for git

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This website was coded by Kirsten McIntyre is open-sourced on Github and
+        hosted on Netlify{" "}
+      </footer>
+    </div>
+  );
+}
