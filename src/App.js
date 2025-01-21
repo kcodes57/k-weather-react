8dfebed5d5d68feb6c9829e92cd8b8385371af6b
kcodes57 <kir652986@gmail.com>
2025-01-21 13:40:51 -0600

    updated links

--- src/App.js
+++ src/App.js
@@ -6,8 +6,26 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <footer>
-        This website was coded by Kirsten McIntyre is open-sourced on Github and
-        hosted on Netlify{" "}
+        Coded by{" "}
+        <a href="https://github.com/kcodes57" target="_blank" rel="noreferrer">
+          Kirsten McIntyre
+        </a>{" "}
+        is open-sourced on{" "}
+        <a
+          href="https://github.com/kcodes57/k-weather-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://k-weather-react.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
       </footer>
     </div>
   );
