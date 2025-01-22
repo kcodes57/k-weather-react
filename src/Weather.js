215caf9b96bed18a96b81337a7ad25b4e76f0cad
kcodes57 <kir652986@gmail.com>
2025-01-22 14:58:57 -0600

    added axios to project and updated with live data

--- src/Weather.js
+++ src/Weather.js
@@ -1,39 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
 import "bootstrap/dist/css/bootstrap.css";
+import axios from "axios";
 
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
-        <input type="search" placeholder="Enter a city" />
-        <input type="submit" value="Search" />
-      </form>
-      <div className="Container">
-        <h1>London</h1>
-        <h3>Monday, 20th September</h3>
-        <img
-          src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-          alt="cloudy"
-        />
-        <div className="row">
-          <ul className="Weather-type col-7">
-            <li className="Temp">
-              4 <span className="Unit">°C</span>
-            </li>
-          </ul>
-          <ul className="col-5">
-            {" "}
-            <li>overcast clouds</li>
-            <li>
-              humidity: <strong>87%</strong>
-            </li>
-            <li>
-              wind: <strong>3.5 km/h</strong>
-            </li>
-          </ul>
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      temperature: response.data.temperature.current,
+      description: response.data.condition.description,
+      icon: response.data.condition.description.icon_url,
+      humidity: response.data.temperature.humidity,
+      wind: response.data.wind.speed,
+    });
+  }
+  let city = "London";
+  let apiKey = "441fcacb9dt020b37114da0ba3e3f5of";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(handleResponse);
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <input type="search" placeholder="Enter a city" />
+          <input type="submit" value="Search" />
+        </form>
+        <div className="Container">
+          <h1>London</h1>
+          <h3>Monday, 20th September</h3>
+          <img src={weatherData.icon} alt={weatherData.description} />
+          <div className="row">
+            <ul className="Weather-type col-7">
+              <li className="Temp">
+                {weatherData.temperature} <span className="Unit">°C</span>
+              </li>
+            </ul>
+            <ul className="col-5">
+              {" "}
+              <li>{weatherData.description}</li>
+              <li>
+                humidity: <strong>{weatherData.humidity}%</strong>
+              </li>
+              <li>
+                wind: <strong>{weatherData.wind} km/h</strong>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    axios.get(apiUrl).then(handleResponse);
+    return "loading";
+  }
 }
