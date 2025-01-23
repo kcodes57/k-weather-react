24b622d19284c279816ce5e84deacc36d5e73197
kcodes57 <kir652986@gmail.com>
2025-01-23 14:25:42 -0600

    created search engine

--- src/Weather.js
+++ src/Weather.js
@@ -4,8 +4,9 @@ import WeatherInfo from "./WeatherInfo";
 import "bootstrap/dist/css/bootstrap.css";
 import axios from "axios";
 
-export default function Weather() {
+export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
   function handleResponse(response) {
     console.log(response.data);
     setWeatherData({
@@ -19,22 +20,36 @@ export default function Weather() {
       date: new Date(response.data.time * 1000),
     });
   }
-  let city = "London";
-  let apiKey = "441fcacb9dt020b37114da0ba3e3f5of";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+  function handleUpdateCity(event) {
+    setCity(event.target.value);
+  }
+
+  function search() {
+    let apiKey = "441fcacb9dt020b37114da0ba3e3f5of";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
 
   if (weatherData.ready) {
     return (
       <div className="Weather">
-        <form>
-          <input type="search" placeholder="Enter a city" />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="search"
+            onChange={handleUpdateCity}
+            placeholder="Enter a city"
+          />
           <input type="submit" value="Search" />
         </form>
         <WeatherInfo data={weatherData} />
       </div>
     );
   } else {
-    axios.get(apiUrl).then(handleResponse);
-    return <div>"loading";</div>;
+    search();
   }
 }
